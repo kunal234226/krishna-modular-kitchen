b1b8506984a40f953cc7edc82ae70fd9066c60b4
kunal234226 <kunal234226@gmail.com>
2026-09-01 20:11:58 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,16 @@
+function userexist(){
+    alert("User already Exist! ")
+};
+
+function invalid(){
+    alert("Invalid username or password")
+}
+
+
+function change_succ(){
+    alert("Password changed successfully!")
+}
+
+function change_unsucc(){
+    alert("Data not found in database ")
+}
